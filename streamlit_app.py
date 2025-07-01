8b8040f65ae7abaa2c7c6c32caa9e8fd2ce94e4e
Jack-Melleuish <jack.melleuish@ausport.gov.au>
2025-07-01 13:27:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ if ingredients_list:
 
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     st.write(ingredients_string)
 
@@ -38,6 +40,4 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!')
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
